7210246d770e0e2284cab311a24dac1ada0b94b3
vtimsit <victortimsit.contact@gmail.com>
2019-01-20 18:55:31 +0100

    :sparkles: Router.js

--- Router.js
+++ Router.js
@@ -19,16 +19,12 @@ class Router
       title: this.datas.documentTitle
     }
 
-    //TEMP
-    this.ajaxResponse = 
-    {
-      DOM: "<div>Hello world</div>",
-      title: "Project"
-    }
+    this.cached = {}
 
     this._listeners()
+    this._checkUrl()
     this._disabledLinks()
-    this._pushState(this.initalResponse, '')
+    this._pushState(this.initalResponse, '/')
     this._runController('')
   }
 
@@ -52,8 +48,28 @@ class Router
   
     const path = _link.getAttribute('href')
 
-    // Launch AJAX request ----> if response run that :
-    this._craftAjaxDOM(this.ajaxResponse, path) // Distinguer les 2 caches: celui dans _getPage() et _pushState()
+    this._craftAjaxDOM(path)
+  }
+
+  _checkUrl()
+  {
+    console.log(window.location.pathname)
+    const pathname = window.location.pathname
+
+    for(let i = 0; i < this.$.links.length; i++)
+    {
+      const src = this.$.links[i].getAttribute('href')
+      console.log(this.$.links[i].getAttribute("href"))
+
+      if(src == pathname)
+      {
+        this._craftAjaxDOM(pathname)
+      }
+      else
+      {
+        this._pushState(this.initalResponse, '/')
+      }
+    }
   }
 
   _dontLeave()
@@ -77,18 +93,10 @@ class Router
     }
   }
 
-  _craftAjaxDOM(_response, _path)
-  {
-    // Push response DOM & path
-    this._pushState(_response, _path)
-
-    this._getPage('pages/' + _path + '.html', '.content', '.content')
-
-    // document.title = _response.title
-    // document.querySelector('.content').innerHTML = _response.DOM
-
-    // Run current controller
-    this._runController(_path)
+  _craftAjaxDOM(_path)
+  { 
+    // Ajax request
+    this._getPage('pages' + _path + '.html', 'body', '.content', _path)
   }
 
   _pushState(_response, _path)
@@ -96,37 +104,109 @@ class Router
     window.history.pushState({ DOM: _response.DOM, documentTitle: _response.title },"", _path)
   }
 
-  _getPage(_url, _from = "body", _to = "body")
+  _getPage(_url, _from = "body", _to = "body", _path)
   {
-    const cached = sessionStorage[_url]
-    
-    _to = document.querySelector(_to) // A string TO turns into an element
-    if(cached){ return _to.innerHTML = cached } // Cache responses for instant re-use re-use
+    let to = {}
 
-    const XHRt = new XMLHttpRequest // New ajax
+    this._waitCursor()
 
-    XHRt.responseType = 'document'  // Ajax2 context and onload() event
-    XHRt.onload = () => { sessionStorage[_url] = _to.innerHTML = XHRt.response.querySelector(_from).innerHTML}
-    XHRt.open("GET", _url, true)
-    XHRt.send()
+    to.DOM = document.querySelector(_to)
+    to.title = document.title
 
-    return XHRt
+    if(this.cached.DOM && this.cached.url == _url)
+    { 
+      document.body.style.cursor = 'auto'
+      to.title = this.cached.title
+      to.DOM.innerHTML = this.cached.DOM 
+
+      this._pushState(this.cached, _path)
+      this._runController(_path)
+    }
+    else
+    {
+      const XHRt = new XMLHttpRequest // New ajax
+  
+      XHRt.responseType = 'document'  // Ajax2 context and onload() event
+      XHRt.onload = () => 
+      { 
+        this._waitCursor(false)
+  
+        this.cached.DOM = to.DOM.innerHTML = XHRt.response.querySelector(_from).innerHTML
+        this.cached.title = document.title = XHRt.response.querySelector('title').innerHTML
+        this.cached.url = _url
+
+        this._pushState(this.cached, _path)
+        this._runController(_path)
+        this._checkUrl()
+      }
+      XHRt.open("GET", _url, true)
+      XHRt.send()
+    }
   }
 
   _runController(_path = '/')
   {
     switch (_path) {
       case '/':
-        console.log('run home controller')
+        // console.log('run home controller')
         break
       case '/project1':
-        console.log('run project1 controller')
+        // console.log('run project1 controller')
         break
       case '/project2':
-        console.log('run project2 controller')
+        // console.log('run project2 controller')
         break
       default:
         break;
+    }
+  }
+
+  _cachedImage()
+  {
+    const images = document.querySelectorAll('img')
+
+    this.cached.images = []
+
+    for(let i = 0; i < images.length; i++)
+    {
+      const cachedImage = new Image()
+      cachedImage.src = images[i].getAttribute('src')
+
+      this.cached.images.push(cachedImage)
+    }
+
+    console.log(this.cached.images)
+  }
+
+  _pullImage()
+  {
+    const images = document.querySelectorAll('img')
+
+    for(let i = 0; i < images.length; i++)
+    {
+      images[i] = this.cached.images[i]
+    }
+  }
+
+  _waitCursor(_state = true)
+  {
+    if(_state)
+    {
+      document.body.style.cursor = 'wait'
+  
+      for(let i = 0; i < this.$.links.length; i++)
+      {
+        this.$.links[i].style.cursor = 'wait'
+      }
+    }
+    else
+    {
+      document.body.style.cursor = 'auto'
+  
+      for(let i = 0; i < this.$.links.length; i++)
+      {
+        this.$.links[i].style.cursor = 'auto'
+      }
     }
   }
 }
